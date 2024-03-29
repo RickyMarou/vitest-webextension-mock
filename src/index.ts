929ed262b49d6cd28488e205e7239ddd514550ec
Marwan Zibaoui <marwan.zibaoui@gmail.com>
2024-03-30 00:07:22 +0100

    wip context

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
 import { vi } from "vitest";
 import { action } from "./action";
+import { type Runtime } from "webextension-polyfill";
+
+interface TestContext {
+  browser: "chrome" | "firefox";
+  os: Runtime.PlatformOs;
+  extensionId: string;
+}
+
+export const context: TestContext = {
+  browser: "chrome",
+  os: "mac",
+  extensionId: "vitest-webextension-mock",
+};
+
+function getURL(path: string) {
+  const scheme =
+    context.browser === "chrome" ? "chrome-extension" : "moz-extension";
+  return `${scheme}://${context.extensionId}/${path}`;
+}
 
 const browser = {
   action,
@@ -7,7 +26,7 @@ const browser = {
     getAll: vi.fn(),
   },
   runtime: {
-    getURL: vi.fn((string) => `chrome-extension://${string}`),
+    getURL: vi.fn((path) => getURL(path)),
     PlatformOs: "mac",
   },
 };
--- test/runtime.test.ts
+++ test/runtime.test.ts
@@ -1,9 +1,20 @@
+import { context } from "../src/index";
 import { describe, expect, test, vi } from "vitest";
 
 describe("browser.runtime", () => {
-  test("getURL", async () => {
-    expect(vi.isMockFunction(browser.runtime.getURL)).toBe(true);
-    await browser.commands.getAll();
-    expect(browser.commands.getAll).toHaveBeenCalledTimes(1);
+  describe("getURL", () => {
+    test("returns a URL", async () => {
+      expect(vi.isMockFunction(browser.runtime.getURL)).toBe(true);
+      expect(browser.runtime.getURL("path")).toBe(
+        "chrome-extension://vitest-webextension-mock/path"
+      );
+    });
+
+    test("changes scheme depending on the context", () => {
+      context.browser = "firefox";
+      expect(browser.runtime.getURL("path")).toBe(
+        "moz-extension://vitest-webextension-mock/path"
+      );
+    });
   });
 });
